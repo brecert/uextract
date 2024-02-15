236fc1450d9ebd5cb8610f9323897897d1f61c36
bree <me@bree.dev>
2024-02-15 06:36:52 -0500

    add basic texture exporting

--- Program.cs
+++ Program.cs
@@ -4,8 +4,11 @@ using System.Text.RegularExpressions;
 using CUE4Parse.Encryption.Aes;
 using CUE4Parse.FileProvider;
 using CUE4Parse.UE4.Assets.Exports;
+using CUE4Parse.UE4.Assets.Exports.Texture;
 using CUE4Parse.UE4.Objects.Core.Misc;
 using CUE4Parse.UE4.Versions;
+using CUE4Parse_Conversion.Textures;
+using SkiaSharp;
 using Newtonsoft.Json;
 using Serilog;
 using Serilog.Sinks.SystemConsole.Themes;
@@ -23,6 +26,7 @@ static class Program
         var objectPath = new Option<string>("--object", "The object path(s) to match for exporting") { IsRequired = true };
         var unrealVersion = new Option<EGame>("--unreal-version", "The unreal engine version the game uses") { IsRequired = true };
         var useRegex = new Option<bool>("--use-regex", "If enabled all objects paths matching the regex in `--object` will be exported");
+        var exportTextures = new Option<SKEncodedImageFormat?>("--textures", "If enabled textures will be exported in the selected format instead");
 
         var rootCommand = new RootCommand("uextract extracts object information from unreal engine games")
         {
@@ -30,11 +34,12 @@ static class Program
             outputDirectory,
             objectPath,
             unrealVersion,
-            useRegex
+            useRegex,
+            exportTextures
         };
 
         rootCommand.SetHandler(
-            (pakDirectory, outputDirectory, objectPath, unrealVersion, useRegex) =>
+            (pakDirectory, outputDirectory, objectPath, unrealVersion, useRegex, exportTextures) =>
             {
                 Log.Logger = new LoggerConfiguration().WriteTo.Console(theme: AnsiConsoleTheme.Literate, restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning).CreateLogger();
 
@@ -53,27 +58,75 @@ static class Program
                         .Where(path => regex.IsMatch(path))
                         .Select(path => (path, objects: provider.LoadAllObjects(path)))
                         .ToList()
-                        .ForEach((info) => ExportObjects(info.objects, info.path, outputDirectory));
+                        .ForEach((info) => Export(info.objects, info.path, outputDirectory, exportTextures));
                 }
                 else
                 {
                     var objects = provider.LoadAllObjects(objectPath);
-                    ExportObjects(objects, objectPath, outputDirectory);
+                    Export(objects, objectPath, outputDirectory, exportTextures);
                 }
             },
-            pakDirectory, outputDirectory, objectPath, unrealVersion, useRegex
+            pakDirectory, outputDirectory, objectPath, unrealVersion, useRegex, exportTextures
         );
 
         rootCommand.Invoke(args);
     }
 
+    static string EnsureOutputPath(DirectoryInfo outputDirectory, string outputPath)
+    {
+        var fullOutputPath = Path.Join(outputDirectory.FullName, outputPath);
+        var directoryPath = Path.GetDirectoryName(fullOutputPath) ?? throw new Exception($"invalid output path for {fullOutputPath}");
+        Console.WriteLine("{0}", Path.GetFullPath(fullOutputPath));
+        Directory.CreateDirectory(directoryPath);
+        return fullOutputPath;
+    }
+
+    static void Export(IEnumerable<UObject> objects, string objectPath, DirectoryInfo outputDirectory, SKEncodedImageFormat? exportTextures)
+    {
+        if (exportTextures is not null)
+        {
+            if (objects.Count() > 1)
+            {
+                Log.Warning($"More than one object at {objectPath}");
+            }
+            ExportObject(objects.First(), objectPath + $"", outputDirectory, exportTextures);
+        }
+        else
+        {
+            ExportObjects(objects, objectPath, outputDirectory);
+        }
+    }
+
+    static void ExportObject(UObject uObject, string objectPath, DirectoryInfo outputDirectory, SKEncodedImageFormat? exportTextures)
+    {
+        if (exportTextures is SKEncodedImageFormat textureFormat && uObject is UTexture2D texture)
+        {
+            ExportTexture(texture, objectPath, outputDirectory, textureFormat);
+        }
+        else
+        {
+            Log.Warning($"UnspecifiedFormat UObject ({uObject.Class}) class for {objectPath}");
+        }
+    }
+
+
+    static void ExportTexture(UTexture2D texture, string objectPath, DirectoryInfo outputDirectory, SKEncodedImageFormat textureFormat)
+    {
+        var outputPath = EnsureOutputPath(outputDirectory, objectPath + "." + textureFormat.ToString().ToLower());
+        using (var pixmap = TextureDecoder.Decode(texture).PeekPixels())
+        {
+            using (var encoded = pixmap.Encode(textureFormat, 90))
+            {
+                // todo: stream this instead of this scary mess :(
+                File.WriteAllBytes(outputPath, encoded.ToArray());
+            }
+        }
+    }
+
     static void ExportObjects(IEnumerable<UObject> objects, string objectPath, DirectoryInfo outputDirectory)
     {
+        var outputPath = EnsureOutputPath(outputDirectory, objectPath + ".json");
         var json = JsonConvert.SerializeObject(objects, Formatting.Indented);
-        var outputPath = Path.Join(outputDirectory.FullName, objectPath + ".json");
-        var directoryPath = Path.GetDirectoryName(outputPath) ?? throw new Exception($"invalid output path for {outputPath}");
-        Console.WriteLine("{0}", Path.GetFullPath(outputPath));
-        Directory.CreateDirectory(directoryPath);
         File.WriteAllText(outputPath, json);
     }
 }
